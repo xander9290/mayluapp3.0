8ebb1b2cb95ad6fe55a8b3aa46057534bdcc883a
alexander <raul.alex.aguiluna@gmail.com>
2021-02-03 02:04:27 -0600

    cerrar ventanas, click derecho disable

--- src/App.js
+++ src/App.js
@@ -1,14 +1,29 @@
+import { useEffect } from "react";
+
 import { operadorSession } from "./helpers";
 
 import LoginForm from "./components/LoginForm";
 import Interface from "./components/Interface";
 
 function App() {
+  useEffect(() => {
+    window.addEventListener("beforeunload", function (e) {
+      e.preventDefault();
+      e.returnValue = "";
+    });
+    document.addEventListener("contextmenu", (e) => e.preventDefault());
+
+    return () => {
+      window.removeEventListener("beforeunload", function (e) {
+        e.preventDefault();
+        e.returnValue = "";
+      });
+      document.removeEventListener("contextmenu", (e) => e.preventDefault());
+    };
+  }, []);
   return (
     <div className="container-fluid">
-      {
-        operadorSession?<Interface />:<LoginForm />
-      }
+      {operadorSession ? <Interface /> : <LoginForm />}
     </div>
   );
 }
